7828428978ef6571b393935d39ccd9069f272924
moamn <mowmnz@gmail.com>
2024-05-12 17:20:48 +0300

    capsule version 12_5
    كلشي شغال بس مشكلة بالبوللت من جانب يونيتي

--- connection.js
+++ connection.js
@@ -1,8 +1,9 @@
 const app = require('./app');
-const io = require('./controller/socket');
+// const io = require('./controller/socket');
+// const io = require('./testroom');
 const mongoose = require('mongoose');
 const server = require('http').Server(app);
-// require('dotenv').config();
+//require('dotenv').config();
 
 const MONGODB_URI =
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.ojvgbto.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`;
@@ -11,7 +12,8 @@ mongoose.connect(MONGODB_URI)
     .then(() =>
     {
         server.listen(process.env.PORT || 3000, () => { console.log('connectttt') });
-        require('./controller/socket').init(server);
+        //require('./controller/socket').init(server);
+        require('./tesstroom1').init(server);
     })
     .catch(error => { console.log('MongoDB connection failed: ', error); });
 
